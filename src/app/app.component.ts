d48e5ccaa6919175a500a83d6592609e87d02923
eskalona17 <fer.escalonso@gmail.com>
2020-09-25 13:01:55 +0200

    deleting employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,33 +2,37 @@ import { Component } from '@angular/core';
 
 import { Empleado } from './models/empleado';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   empleadosArray: Empleado[] = [
-    {id: 1, nombre: "Paco", pais: "España"},
-    {id: 2, nombre: "Manolo", pais: "España"},
-    {id: 3, nombre: "Ramón", pais: "España"}
+    { id: 1, nombre: 'Paco', pais: 'España' },
+    { id: 2, nombre: 'Manolo', pais: 'España' },
+    { id: 3, nombre: 'Ramón', pais: 'España' },
   ];
 
   //creamos un nuevo empleado en blanco
   EmpleadoSeleccionado: Empleado = new Empleado();
 
-  openForEdit(empleado: Empleado){
+  openForEdit(empleado: Empleado) {
     this.EmpleadoSeleccionado = empleado;
   }
 
-  addOrEdit(){
-    this.EmpleadoSeleccionado.id = this.empleadosArray.length + 1;
-    this.empleadosArray.push(this.EmpleadoSeleccionado);
-    console.log(this.empleadosArray);
-
+  addOrEdit() {
+    if (this.EmpleadoSeleccionado.id === 0) {
+      this.EmpleadoSeleccionado.id = this.empleadosArray.length + 1;
+      this.empleadosArray.push(this.EmpleadoSeleccionado);
+    }
     this.EmpleadoSeleccionado = new Empleado();
-    
   }
-
+  
+  delete(){
+    if(confirm('estás seguro de querer eliminarlo?')){
+      this.empleadosArray = this.empleadosArray.filter(x => x != this.EmpleadoSeleccionado);
+    this.EmpleadoSeleccionado = new Empleado();
+    }
+  }
 }
