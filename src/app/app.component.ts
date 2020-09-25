cc6102538c4d10f8370f60ddcc387d4008dccec3
eskalona17 <fer.escalonso@gmail.com>
2020-09-25 12:18:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Empleado } from './models/empleado';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-angular-app';
+  empleadosArray: Empleado[] = [
+    {id: 1, nombre: "Paco", pais: "España"},
+    {id: 2, nombre: "Manolo", pais: "España"},
+    {id: 3, nombre: "Ramón", pais: "España"},
+  ];
+
+  EmpleadoSeleccionado: Empleado
+
 }
