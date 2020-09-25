b17d3fc88b03e809c4440bc73b241d1f0b85ca11
eskalona17 <fer.escalonso@gmail.com>
2020-09-25 12:55:02 +0200

    form done and listing employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,23 @@ export class AppComponent {
   empleadosArray: Empleado[] = [
     {id: 1, nombre: "Paco", pais: "España"},
     {id: 2, nombre: "Manolo", pais: "España"},
-    {id: 3, nombre: "Ramón", pais: "España"},
+    {id: 3, nombre: "Ramón", pais: "España"}
   ];
 
-  EmpleadoSeleccionado: Empleado
+  //creamos un nuevo empleado en blanco
+  EmpleadoSeleccionado: Empleado = new Empleado();
+
+  openForEdit(empleado: Empleado){
+    this.EmpleadoSeleccionado = empleado;
+  }
+
+  addOrEdit(){
+    this.EmpleadoSeleccionado.id = this.empleadosArray.length + 1;
+    this.empleadosArray.push(this.EmpleadoSeleccionado);
+    console.log(this.empleadosArray);
+
+    this.EmpleadoSeleccionado = new Empleado();
+    
+  }
 
 }
